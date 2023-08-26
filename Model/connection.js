d186f53759a53530b9110078e531158b55bd4104
waliu <waliuraheemprobity@gmail.com>
2023-08-26 16:31:02 +0100

    deployment

--- Model/connection.js
+++ Model/connection.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database:"probity_libsys"
+    // host: "localhost",
+    // user: "root",
+    // password: "",
+    // database:"probity_libsys"
+     host:"db4free.net",
+     user:"waliuraheem",
+     password: "probitymr",
+     database: "probity_libsys",
+     port: 3306
+
 });
 connection.connect();
 console.log("database is connected!");
