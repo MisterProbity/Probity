ed059ad4d0e0aae8e014390395baa5462ff455ed
waliu <waliuraheemprobity@gmail.com>
2023-08-28 20:19:11 +0100

    ChangedValid

--- Model/admin.js
+++ Model/admin.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs')
 
 class Admin extends Model{
     static async login (email, pass){
-        let sql = `SELECT * FROM Admins WHERE admins.email = ?`
+        let sql = `SELECT * FROM admins WHERE admins.email = ?`
         const [result] = await conn.execute(sql,[email])
         if(result.length>0){
             let admin = new Admin(result[0]);
